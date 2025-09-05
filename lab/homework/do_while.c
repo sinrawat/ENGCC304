7e8b604db68d44ff357afc3b8568af89c4105b18
sinrawat <sinrawatxzz999@gmail.com>
2025-09-05 10:25:06 +0700

    lab

---
+++ lab/homework/do_while.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, isPrime;
+
+    printf("Enter number : ");
+    scanf("%d", &n);
+
+    i = n;
+    if (i >= 2) {
+        do {
+            isPrime = 1;
+            j = 2;
+            while (j * j <= i) {
+                if (i % j == 0) {
+                    isPrime = 0;
+                    break;
+                }
+                j++;
+            }
+            if (isPrime) {
+                printf("%d ", i);
+            }
+            i--;
+        } while (i >= 2);
+    }
+
+    return 0;
+}
---
+++ lab/homework/while.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, isPrime;
+
+    printf("Enter number : ");
+    scanf("%d", &n);
+
+    i = n;
+    while (i >= 2) {
+        isPrime = 1;
+        j = 2;
+        while (j * j <= i) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+            j++;
+        }
+        if (isPrime) {
+            printf("%d ", i);
+        }
+        i--;
+    }
+
+    return 0;
+}
